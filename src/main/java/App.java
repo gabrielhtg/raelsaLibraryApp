8d21c82de8602a9376974579160dfb678b72b033
ifs21010-itdel <ifs21010@students.del.ac.id>
2022-11-28 10:57:41 +0700

    percobaan

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        new HomeDisplay("Gabriel Cesar Hutagalung", "ganteng");
-        // new LoginDisplay();
+        // new HomeDisplay("Gabriel Cesar Hutagalung", "ganteng");
+        new LoginDisplay();
         // new TambahBuku();
         // new Detail();
         // new InfoMember();
